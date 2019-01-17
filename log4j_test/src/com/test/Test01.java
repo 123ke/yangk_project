e126cef81e845e6c996b959b31f0a4642bcdbebd
123ke <2126772515@qq.com>
2019-01-17 15:49:52 +0800

    提交注释

--- log4j_test/src/com/test/Test01.java
+++ log4j_test/src/com/test/Test01.java
@@ -5,5 +5,5 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello world...");
 		System.out.println("hello java...");		
-	}
+	}//https://github.com/123ke/yangk_project/invitations
 }
